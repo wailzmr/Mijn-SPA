409988df23b8371438906573ceb31181c7213d46
Wail Zemouri <wail.zemouri@student.ehb.be>
2025-05-13 18:28:56 +0200

    adjustments html-code#

--- src/components/card.js
+++ src/components/card.js
@@ -6,7 +6,9 @@ export function renderCharacterCard(character) {
   clone.querySelector("img").src = character.image;
   clone.querySelector("h2").textContent = character.name;
   clone.querySelector(".species").textContent = character.species;
-  clone.querySelector(".location").textContent = character.location.name;
+  clone.querySelector(".location").textContent = `Location: ${character.location.name}`;
+  clone.querySelector(".status").textContent = `Status: ${character.status}`;
+  clone.querySelector(".gender").textContent = `Gender: ${character.gender}`;
 
   const favBtn = clone.querySelector(".favorite-button");
   favBtn.textContent = isFavorite(character.id) ? "💖" : "⭐";
