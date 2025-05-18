d1152425c68ddbbabdd9cb6abab090ee539dac59
wailzmr <wail.zemouri@student.ehb.be>
2025-05-18 23:43:05 +0200

    rickandmorty api adjustment
    ik besloot dat ik meer dan 20 characters wou dus ik heb mijn code aangepast om hieraan te geraken

--- src/api/rickmorty.js
+++ src/api/rickmorty.js
@@ -1,7 +1,14 @@
 const BASE_URL = 'https://rickandmortyapi.com/api/character';
 
-export async function getCharacters(page = 1) {
-  const response = await fetch(`${BASE_URL}?page=${page}`);
-  const data = await response.json();
-  return data.results;
+export async function getCharacters() { /* er zijn oorspronkelijk 20 characters per page */ 
+   
+  const characters = [];               /*ik wou een 100tal characters ophalen en hiervoor moest ik 5 pagina's ophalen   */     
+
+  for (let page = 1; page <= 5; page++) {
+    const response = await fetch(`${BASE_URL}?page=${page}`);
+    const data = await response.json();
+    characters.push(...data.results);
+  }
+
+  return characters;
 }
